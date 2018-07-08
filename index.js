44ef7dc244b49e67d4acc58735806cc4cd0d7a45
Jakub Biesek <jakub.webdesign@gmail.com>
2018-07-08 17:45:49 +0100

    add mongoose connection

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import routes from './routes/routes';
-
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 3015;
+
+//add mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/NodeAppDb', {
+    useMongoClient: true
+});
+
 routes(app);
 
 app.get('/', (req, res) => {
     res.send('Server is running at port ' + port);
 });
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
