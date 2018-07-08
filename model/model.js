7bf37c88171a95cb2ca42d9a3c2eac02155eb8d3
Jakub Biesek <jakub.webdesign@gmail.com>
2018-07-08 18:01:49 +0100

    add model schema to mongo db with parameters

--- model/model.js
+++ model/model.js
@@ -3,4 +3,23 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 
-
+export const ContactSchema = new Schema({
+    firstName: {
+        type: String,
+        required: 'Type you Name'
+    },
+    lastName: {
+        type: String,
+        required: 'Type you Last Name'
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: Number,
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
