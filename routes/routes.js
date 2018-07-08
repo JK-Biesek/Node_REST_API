7685c74e503bcf2c9086c0fbaf284ece5ee518f0
Jakub Biesek <jakub.webdesign@gmail.com>
2018-07-08 14:00:38 +0100

    add next call to routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,25 @@
-const routes = function(app){
+const routes = function (app) {
     app.route('/contact')
-    .get((req,res)=>{
-        res.send('GET request done')
-    })
-    .post((req,res)=>{
-        res.send('Post request done status ok')
-    });
+        .get((req, res, next) => {
+            console.log(`request from ${req.originalUrl} and type is ${req.method}`);
+            next();
+        }, (req, res) => {
+            res.send('GET request done')
+        })
+        .post((req, res) => {
+            res.send('Post request done status ok')
+        });
 
     app.route('/contact/:contactID')
-    .put((req,res)=>{
-        res.send(`PUT request done`)
-    })
-    .delete((req,res)=>{
-        res.send('Delete request done status ok')
-    });
+        .put((req, res,next) => {
+            console.log(`request from ${req.originalUrl} and type is ${req.method}`);
+            next();
+        },(req,res) =>{
+            res.send(`PUT request done`)
+        })
+        .delete((req, res) => {
+            res.send('Delete request done status ok')
+        });
 
 }
 export default routes;
